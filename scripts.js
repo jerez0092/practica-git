168fbfaf21c24201533d715ac8cd3dbbf1f09b69
jerez0092 <luismolina0092@gmail.com>
2024-12-02 11:02:26 -0600

    cambios 1

---
+++ scripts.js
@@ -0,0 +1,20 @@
+let a = 5;
+let b = 10;
+let suma = a + b;
+console.log("La total de la suma es:" + suma);
+
+let primeraparte = "hola";
+let segundaparte = "mundo";
+console.log(primeraparte + segundaparte);
+
+let largo = 8;
+let ancho = 5;
+let area = largo * ancho;
+console.log("el area resultante es:" + area);
+
+let x = 3;
+let y = 7;
+let temp = x;
+x = y;
+y = temp;
+console.log("Resultado final de X=3 y Y=7 despues del cambio es: X= "+X+", Y="+y+"");
